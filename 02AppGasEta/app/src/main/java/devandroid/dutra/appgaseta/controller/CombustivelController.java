b3156bd93e83719b338ee8a6b24198f1d7701724
virusDutra <94208590+virusDutra@users.noreply.github.com>
2024-01-25 07:38:56 -0300

    Craindo DB do app,Salvando objetos no banco de dados. metodo salvar e salvarObjetos

--- 02AppGasEta/app/src/main/java/devandroid/dutra/appgaseta/controller/CombustivelController.java
+++ 02AppGasEta/app/src/main/java/devandroid/dutra/appgaseta/controller/CombustivelController.java
@@ -1,11 +1,13 @@
 package devandroid.dutra.appgaseta.controller;
 
+import android.content.ContentValues;
 import android.content.SharedPreferences;
 
+import devandroid.dutra.appgaseta.database.GasEtaDB;
 import devandroid.dutra.appgaseta.model.Combustivel;
 import devandroid.dutra.appgaseta.view.GasEtaActivity;
 
-public class CombustivelController {
+public class CombustivelController extends GasEtaDB {
 
     SharedPreferences preferences;
 
@@ -14,6 +16,7 @@ public class CombustivelController {
     public static final String NOME_PREFERENCES ="pref_gaseta";
 
     public CombustivelController(GasEtaActivity activity){
+        super (activity);
 
         preferences = activity.getSharedPreferences(NOME_PREFERENCES, 0);
 
@@ -23,10 +26,20 @@ public class CombustivelController {
 
     public void salvar(Combustivel combustivel){
 
+        ContentValues dados = new ContentValues();
+
         dadosPreferences.putString("combustivel", combustivel.getNomeDoCombustivel());
         dadosPreferences.putFloat("precoDoCombustivel", (float)combustivel.getPrecoDoCombustivel());
         dadosPreferences.putString("recomendacao", combustivel.getRecomendacao());
         dadosPreferences.apply();
+
+        dados.put("nomeDoCombustivel", combustivel.getNomeDoCombustivel());
+        dados.put("precoDoCombustivel", combustivel.getPrecoDoCombustivel());
+        dados.put("recomendacao", combustivel.getRecomendacao());
+
+        salvarObjeto("combustivel", dados);
+
+
 
     }
 
--- 02AppGasEta/app/src/main/java/devandroid/dutra/appgaseta/database/GasEtaDB.java
+++ 02AppGasEta/app/src/main/java/devandroid/dutra/appgaseta/database/GasEtaDB.java
@@ -1,5 +1,6 @@
 package devandroid.dutra.appgaseta.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -11,8 +12,8 @@ import androidx.annotation.Nullable;
 
 public class GasEtaDB extends SQLiteOpenHelper {
 
-    public static final String DB_NAME = "gaseta.db";
-    public static final int DB_VERSION = 1;
+    private static final String DB_NAME = "gaseta.db";
+    private static final int DB_VERSION = 1;
 
     Cursor cursor;
 
@@ -45,5 +46,9 @@ public class GasEtaDB extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
+    }
+
+    public void salvarObjeto (String tabela, ContentValues dados){
+        db.insert(tabela, null, dados);
     }
 }
